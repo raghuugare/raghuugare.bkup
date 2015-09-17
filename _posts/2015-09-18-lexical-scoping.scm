095a9a22ffd340d4b06d9d3deea3eb34b8a1b85b
Raghu Ugare <raghu.ugare@gmail.com>
2015-09-18 00:20:33 +0530

    The seed of a nice new article on 	a feature of Scheme...

---
+++ _posts/2015-09-18-lexical-scoping.scm
@@ -0,0 +1,12 @@
+---
+layout: post
+title:  "Lexical Scoping -- first class"
+date:   2015-09-18 12:21:21
+categories: programming
+draft: true
+
+---
+
+## Lexical Scoping in Scheme
+
+Write about the historical context, & how Scheme implemented _'Lexical Scoping'_, borrowing this idea from ALGOL-60
